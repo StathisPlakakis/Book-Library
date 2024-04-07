d74c5eb1a43b23ec899c30685885397de69b3dcb
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-04-07 18:18:52 +0300

    Add a dialog form
    Add and style the dialog allowing users to input the details of  new book

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const myLibrary = [];
 const addButton = document.querySelector(".add");
 const tableBody = document.querySelector("tbody");
+const dialog = document.querySelector("dialog");
+
 
 
 function Book (title, author, pages, read) {
@@ -17,6 +19,7 @@ function Book (title, author, pages, read) {
 function addBookToLibrary(title, author, pages, read) {
    let newBook = new Book(title, author, pages, read);
    myLibrary.push(newBook);
+   console.log(newBook);
 }
 
 function arrayTraversal (array) {
@@ -43,10 +46,12 @@ function arrayTraversal (array) {
 }
 
 addButton.addEventListener("click", () => {
-    const title = prompt("Give me title");
-    const author = prompt("Give me author");
-    const pages = prompt("Give me pages");
-    const read = prompt("Read (y/n)");
-    addBookToLibrary(title, author, pages, read);
-    arrayTraversal(myLibrary);
+    dialog.showModal();
 })
+
+// const title = prompt("Give me title");
+//     const author = prompt("Give me author");
+//     const pages = prompt("Give me pages");
+//     const read = prompt("Read (y/n)");
+//     addBookToLibrary(title, author, pages, read);
+//     arrayTraversal(myLibrary);
